1718ee56c5f5f4b369ceef1754e9ab6b29c95088
newbradb <newbradburyfan@gmail.com>
2026-08-07 09:49:34 +0300

    exercise with simple print function

---
+++ bottles.py
@@ -0,0 +1,8 @@
+def bottle_verse ():
+    for i in range (99, 0, -1 ):
+        print( i, ' bottles of beer on the wall')
+        print( i, ' bottles of beer' )
+        print('Take one down, pass it around')
+        print(i - 1, ' 98 bottles of beer on the wall')
+
+bottle_verse()
