11556d835f7140ea7e6f987ab6ee1b353932ef06
newbradb <newbradburyfan@gmail.com>
2026-08-07 09:52:59 +0300

    redo function to read arguments from user

--- bottles.py
+++ bottles.py
@@ -1,8 +1,9 @@
 def bottle_verse ():
-    for i in range (99, 0, -1 ):
+    s = input ('How long input is ?')
+    for i in range (s, 0, -1 ):
         print( i, ' bottles of beer on the wall')
         print( i, ' bottles of beer' )
         print('Take one down, pass it around')
-        print(i - 1, ' 98 bottles of beer on the wall')
+        print(i - 1, ' bottles of beer on the wall')
 
 bottle_verse()
