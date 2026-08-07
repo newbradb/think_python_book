28d32604be024949bafab71a9de9095c5430c654
newbradb <newbradburyfan@gmail.com>
2026-08-07 13:38:44 +0300

    square via function

---
+++ interfaces.py
@@ -0,0 +1,13 @@
+import turtle
+
+from turtle import forward
+from turtle import left, right
+
+
+def square():
+    for i in range(4):
+        forward(50)
+        left(90)
+    turtle.done ()
+
+square()
