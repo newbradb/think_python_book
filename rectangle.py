7bf2e0d2eddc87c714dd6aea4a467a23784584eb
newbradb <newbradburyfan@gmail.com>
2026-08-10 12:50:17 +0300

    final version of the exercise

--- rectangle.py
+++ rectangle.py
@@ -4,8 +4,14 @@ from turtle import forward, left
 
 def rectangle():
     wide = input ('Input the wide of rectangle: ')
+    while not wide.isdigit() :
+        print ('Please enter the valid number')
+        wide = input ('Input the wide of rectangle: ')
     wide = int (wide)
     tall = input('Input the tall of rectangle: ')
+    while not tall.isdigit() :
+        print ('Please enter the valid number')
+        tall = input ('Input the tall of rectangle: ')
     tall = int(tall)
 
     forward(wide)
