846ad6472829dfd1aed27b006bb8d01b9772aaf6
newbradb <newbradburyfan@gmail.com>
2026-08-10 09:51:21 +0300

    script to draw a circle

---
+++ approximating_circle.py
@@ -0,0 +1,19 @@
+import math
+import turtle
+from turtle import forward, left
+
+def polygon (n, length):
+    angle = 360 / n
+    for i in range (n):
+        forward (length)
+        left (angle)
+
+def circle (radius):
+    circumference = 2 * math.pi * radius
+    n = 30 
+    length = circumference / n
+    polygon (n, length)
+
+circle (radius=30)
+turtle.speed = 0.02
+turtle.done()
