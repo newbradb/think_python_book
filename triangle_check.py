d52a8948ee2e40cfcff1c37e2a06b9fdc71febe3
newbradb <newbradburyfan@gmail.com>
2026-08-11 11:28:30 +0300

    parentesis correction

--- triangle_check.py
+++ triangle_check.py
@@ -1,3 +1,4 @@
+from sys import exit
 
 def is_triangle (a , b, c):
     if c > a + b:
@@ -9,4 +10,23 @@ def is_triangle (a , b, c):
     else:
         print('Yes, this is suitable for triangle')
 
-is_triangle (5, 5, 15)
+def validate_args():
+    one = input('Please specify the first line:')
+    two = input('Please specify the second line:')
+    three = input('Please specify the third line:')
+
+    if one.isdigit() != 1:
+        print('Only integers allowed!')
+        exit()
+    elif two.isdigit() != 1: 
+        print('Only integers allowed!')
+        exit()
+    elif three.isdigit() !=1:
+        print('Only integers allowed')
+        exit ()
+    return one, two, three
+
+
+a, b, c = validate_args ()
+
+is_triangle(a, b, c)
