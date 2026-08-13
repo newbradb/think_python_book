fd314524bbed356374b858ae91e67f2e693ec422
newbradb <newbradburyfan@gmail.com>
2026-08-13 13:31:55 +0300

    script to search words without E letter

---
+++ gadsby.py
@@ -0,0 +1,10 @@
+found_e = False
+
+for letter in 'Gadsby':
+    if letter == 'E' or letter == 'e':
+        found_e = True 
+
+if found_e :
+    print('The word has an "e"')
+else:
+    print('No letter "e"')
