83df668a92d2c6bb3976b4c0dd814f90b686b007
newbradb <newbradburyfan@gmail.com>
2026-08-13 13:40:49 +0300

    search e script updated with function

--- gadsby.py
+++ gadsby.py
@@ -1,10 +1,13 @@
-found_e = False
 
-for letter in 'Gadsby':
-    if letter == 'E' or letter == 'e':
-        found_e = True 
+def search_e (word):
+    found_e = False
+    for letter in word:
+        if letter == 'E' or letter == 'e':
+            found_e = True 
 
-if found_e :
-    print('The word has an "e"')
-else:
-    print('No letter "e"')
+    if found_e :
+        print('The word has an "e"')
+    else:
+        print('No letter "e"')
+        
+search_e ('sky')
