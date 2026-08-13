b6e4e470e4c94aeb8051efa4dad16e81dd334f82
newbradb <newbradburyfan@gmail.com>
2026-08-13 17:05:30 +0300

    counter of e

--- test_words.py
+++ test_words.py
@@ -1,7 +1,17 @@
 total = 0 
+count = 0
+
+def has_e(word):
+    for i in word:
+        if i == 'E' or i == 'e':
+            return True
+    return False
+
 
 for line in open ('words.txt'):
     word = line.strip()
     total += 1
+    if has_e(word):
+        count += 1
 
-print(total)
+print(count)
