9e98f283b819f9b00299c0ea1274db006e5090d7
newbradb <newbradburyfan@gmail.com>
2026-08-14 10:43:26 +0300

    added docstring tests

--- test_words.py
+++ test_words.py
@@ -1,13 +1,24 @@
+from doctest import run_docstring_examples
+
 total = 0 
 count = 0
 
 def has_e(word):
     return 'e' in word.lower()
-            
-for line in open ('words.txt'):
-    word = line.strip()
-    total += 1
-    if has_e(word):
-        count += 1
 
-print(count)
+def uses_any(word, letters):
+    """CHECKS if a word uses any of a list letters 
+    >>> uses_any('banana','aeiou')
+    True
+    >>> uses_any('apple', 'xyz')
+    False
+    """
+
+    for letter in word.lower():
+        if letter in letters.lower():
+            return True
+    return False
+
+run_docstring_examples(uses_any, globals(), verbose=True)
+
+
