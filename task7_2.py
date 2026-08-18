1f8054e0e4a3c546fab91e4f1d8fc31a9361eeb8
newbradb <newbradburyfan@gmail.com>
2026-08-18 14:00:13 +0300

    corrected search

--- task7_2.py
+++ task7_2.py
@@ -1,3 +1,4 @@
+from doctest import run_docstring_examples
 
 def uses_only(word, available):
     """Checks whether a word uses only the available letters.
@@ -6,5 +7,13 @@ def uses_only(word, available):
     True
     >>> uses_only('apple', 'apl')
     False
+    >>> uses_only('care', 'car')
+    False
     """
-    return None
+
+    for i in word.lower():
+        if   i not in available.lower():
+            return False
+    return True
+
+run_docstring_examples (uses_only, globals(), verbose=True)
