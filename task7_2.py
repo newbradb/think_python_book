7906db77b5d28a1126e58d6ca039b4c5b5cc8f33
newbradb <newbradburyfan@gmail.com>
2026-08-18 12:53:00 +0300

    started another task

---
+++ task7_2.py
@@ -0,0 +1,10 @@
+
+def uses_only(word, available):
+    """Checks whether a word uses only the available letters.
+    
+    >>> uses_only('banana', 'ban')
+    True
+    >>> uses_only('apple', 'apl')
+    False
+    """
+    return None
