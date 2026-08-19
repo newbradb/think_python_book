e064d26780833e32ae63d7f410db0b4f09281490
newbradb <newbradburyfan@gmail.com>
2026-08-19 10:32:52 +0300

    function to count scores

--- task_7_4.py
+++ task_7_4.py
@@ -17,13 +17,46 @@ def check_word(word, avaliable, required):
     if len(word) <= 3:
         return False
 
-    if required.lower() not in word.lower():
-        return False
+    for letter in required.lower():
+         if letter not in word.lower():
+            return False
 
     for letter in word.lower():
         if letter not in avaliable.lower():
             return False
 
     return True
+
+def word_score(word, avaliable): 
+    """Compute the score for acceptable word.
+
+    >>> word_score('card', ACDLORT)
+    1
+    >>> word_score('color', ACDLORT)
+    5
+    >>> word_score('cartload', ACDLORT)
+    15    
+    """
+    score = 0 
+
+    for letter in word.lower():
+            if letter not in avaliable.lower():
+                raise ValueError ('No letters avaliable in the word')
+
+    if len (word) == 4:
+        if avaliable.lower in word.lower():
+            score = 1
+
+    if len (word) > 4:
+         if avaliable.lower in word.lower():
+             score += 1
+
+    for i in word:
+        if i not in word.lower():
+            return False
+    score += 7
+
+    
+
 
 run_docstring_examples (check_word, globals(), verbose=True)
